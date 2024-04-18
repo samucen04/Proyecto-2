7034ad18ab6a596ae5dc38e8b82cec75f69414b7
samucen04 <142962650+samucen04@users.noreply.github.com>
2024-04-17 23:47:41 -0500

    .

--- TypeScript1/typescripts/series.ts
+++
@@ -1,8 +0,0 @@
-export class Serie
-{
-    index: any;
-    constructor(public id: number, public name: string, public channel: string, public seasons: number, public info: string, public pagina: string, public imgLink: string)
-    {
-
-    }
-}
